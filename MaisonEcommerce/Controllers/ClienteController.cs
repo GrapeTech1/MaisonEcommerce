84e216b0a049c62ad97bcfdd578dbd79cc33199d
Debora <ds336756@gmail.com>
2025-05-23 13:36:44 -0300

    Criando as controllers de Cliente, funcionário e login

---
+++ MaisonEcommerce/Controllers/ClienteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MaisonEcommerce.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MaisonEcommerce/Controllers/FuncionarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MaisonEcommerce.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
