25bf7a21e6dce0e66cad473502e803e3fe30173f
Debora <ds336756@gmail.com>
2025-05-26 03:44:54 -0300

    Consertando models e Adicionando a estrutura básica das views do cliente

--- MaisonEcommerce/Models/Plano.cs
+++ MaisonEcommerce/Models/Plano.cs
@@ -7,7 +7,7 @@ namespace MaisonEcommerce.Models
         public int IdPlano { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
-        public DuracaoPlano Duracao { get; set; }
+        public string DuracaoPlano { get; set; }
         public decimal Preco { get; set; }
         public DateTime DataCadastro { get; set; }
         public DateTime DataAtualizacao { get; set; }
