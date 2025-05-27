0c82897c6ad446a785788547022faa89738d131e
Debora <ds336756@gmail.com>
2025-05-27 02:45:43 -0300

    colocando not null na model do Servico

--- MaisonEcommerce/Models/Servico.cs
+++ MaisonEcommerce/Models/Servico.cs
@@ -5,8 +5,8 @@ namespace MaisonEcommerce.Models
     public class Servico
     {
         public int IdServico { get; set; }
-        public string Nome { get; set; }
-        public string Descricao { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
         public decimal Preco { get; set; }
         public DateTime DataCadastro { get; set; }
         public DateTime DataAtualizacao { get; set; }
