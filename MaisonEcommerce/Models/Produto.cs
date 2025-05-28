289b79974901cff25b887d16a11e669b9f5b16b8
Debora <ds336756@gmail.com>
2025-05-28 14:22:42 -0300

    Ajeitando a model do produto e criando o repositório do produto

--- MaisonEcommerce/Models/Produto.cs
+++ MaisonEcommerce/Models/Produto.cs
@@ -5,9 +5,9 @@ namespace MaisonEcommerce.Models
     public class Produto
     {
         public int IdProduto { get; set; }
-        public string Nome { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
         public int Quantidade { get; set; }
-        public string Descricao { get; set; }
         public decimal Preco { get; set; }
         public DateTime DataCadastro { get; set; }
         public DateTime DataAtualizacao { get; set; }
