ac6d2d4c6e504fa01a0f7e5822b22e9efb5545ec
Debora <ds336756@gmail.com>
2025-05-28 15:16:06 -0300

    Adicionando o código do repositório e controller do produto

---
+++ MaisonEcommerce/Controllers/ProdutoController.cs
@@ -0,0 +1,94 @@
+﻿using MaisonEcommerce.Models;
+using MaisonEcommerce.Repositorio;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaisonEcommerce.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private readonly ProdutoRepositorio _produtoRepositorio;
+
+        public ProdutoController(ProdutoRepositorio produtoRepositorio)
+        {
+            _produtoRepositorio = produtoRepositorio;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_produtoRepositorio.TodosProdutos());
+        }
+
+        public IActionResult CadastrarProduto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CadastrarProduto(Produto produto)
+        {
+            int linhasAfetadas = _produtoRepositorio.Cadastrar(produto);
+
+            if (linhasAfetadas > 0)
+            {
+                TempData["Mensagem"] = "Produto cadastrado com sucesso!";
+                TempData["Classe"] = "alert alert-success";
+                return RedirectToAction(nameof(Index));
+            }
+
+            else
+            {
+                TempData["Mensagem"] = "O produto já existe no sistema.";
+                TempData["Classe"] = "alert alert-danger";
+                return View();
+            }
+            
+        }
+
+        public IActionResult EditarProduto(int id)
+        {
+            var produto = _produtoRepositorio.ObterProduto(id);
+
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            return View(produto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult EditarProduto(int id, [Bind("IdProduto, Nome, Descricao, Quantidade, Preco")] Produto produto)
+        {
+           if (id != produto.IdProduto)
+           {
+               return BadRequest();
+           }
+
+           if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (_produtoRepositorio.Atualizar(produto))
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+
+                catch (Exception)
+                {
+                    ModelState.AddModelError("", "Erro ao editar o produto.");
+                    return View(produto);
+                }
+            }
+
+            return View(produto);
+        }
+
+        public IActionResult ExcluirProduto(int id)
+        {
+            _produtoRepositorio.Excluir(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- MaisonEcommerce/Repositorio/ProdutoRepostorio.cs
+++ MaisonEcommerce/Repositorio/ProdutoRepostorio.cs
@@ -1,6 +1,130 @@
-﻿namespace MaisonEcommerce.Repositorio
+﻿using MaisonEcommerce.Models;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace MaisonEcommerce.Repositorio
 {
-    public class ProdutoRepostorio
+    public class ProdutoRepositorio(IConfiguration configuration)
     {
+        private readonly string _conexaoMySQL = configuration.GetConnectionString("ConexaoMySQL");
+
+        public int Cadastrar(Produto produto)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand("Call isertProduto(@nome, @descricao, @quantidade, @preco);", conexao);
+                cmd.Parameters.Add("@nome", MySqlDbType.VarChar).Value = produto.Nome;
+                cmd.Parameters.Add("@descricao", MySqlDbType.VarChar).Value = produto.Descricao;
+                cmd.Parameters.Add("@quantidade", MySqlDbType.Int32).Value = produto.Quantidade;
+                cmd.Parameters.Add("@preco", MySqlDbType.Decimal).Value = produto.Preco;
+
+                int linhasAfetadas = cmd.ExecuteNonQuery();
+
+                conexao.Close();
+
+                return linhasAfetadas;
+            }
+        }
+
+        public bool Atualizar(Produto produto)
+        {
+            try
+            {
+                using (var conexao = new MySqlConnection(_conexaoMySQL))
+                {
+                    conexao.Open();
+
+                    MySqlCommand cmd = new MySqlCommand("Update tb_Produto set Nome=@nome, Descricao=@descricao, Quantidade=@quantidade, Preco=@preco", conexao);
+                    cmd.Parameters.Add("@idProduto", MySqlDbType.Int32).Value = produto.IdProduto;
+                    cmd.Parameters.Add("@nome", MySqlDbType.VarChar).Value = produto.Nome;
+                    cmd.Parameters.Add("@descricao", MySqlDbType.VarChar).Value = produto.Descricao;
+                    cmd.Parameters.Add("@quantidade", MySqlDbType.Int32).Value = produto.Quantidade;
+                    cmd.Parameters.Add("@preco", MySqlDbType.Decimal).Value = produto.Preco;
+
+                    int linhasAfetadas = cmd.ExecuteNonQuery();
+                    return linhasAfetadas > 0;
+                }
+            }
+
+            catch (MySqlException ex)
+            {
+                Console.WriteLine($"Erro ao editar produto: {ex.Message}");
+                return false;
+            }
+        }
+
+        public IEnumerable<Produto> TodosProdutos()
+        {
+            List<Produto> produtos = new List<Produto>();
+
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand("select * from tb_Produto", conexao);
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                conexao.Close();
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    produtos.Add(
+                        new Produto
+                        {
+                            IdProduto = Convert.ToInt32(dr["IdProduto"]),
+                            Nome = ((string)dr["Nome"]),
+                            Descricao = ((string)dr["Descricao"]),
+                            Quantidade = Convert.ToInt32(dr["Quantidade"]),
+                            Preco = Convert.ToDecimal(dr["Preco"]),
+                        }
+                        );
+                }
+                return produtos;
+            }
+        }
+
+        public Produto ObterProduto(int codigo)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand("select * from tb_Produto where IdProduto = @codigo", conexao);
+                cmd.Parameters.AddWithValue("@codigo", codigo);
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                MySqlDataReader dr;
+                Produto produto = new Produto();
+
+                dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+                while (dr.Read())
+                {
+                    produto.IdProduto = Convert.ToInt32(dr["IdProduto"]);
+                    produto.Nome = (string)(dr["Nome"]);
+                    produto.Descricao = (string)(dr["Descricao"]);
+                    produto.Quantidade = Convert.ToInt32(dr["Quantidade"]);
+                    produto.Preco = Convert.ToDecimal(dr["Preco"]);
+                }
+                return produto;
+            }
+        }
+
+        public void Excluir(int IdProduto)
+        {
+            using (var conexao  = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand("delete from tb_Produto where IdProduto = @IdProduto", conexao);
+                cmd.Parameters.AddWithValue("@IdProduto", IdProduto);
+                int linhasAfetadas = cmd.ExecuteNonQuery();
+
+                conexao.Close();
+            }
+        }
     }
 }
