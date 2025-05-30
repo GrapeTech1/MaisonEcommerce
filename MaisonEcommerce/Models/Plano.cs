e1ecca5fb15ef26b27ad0affb37276fbc6899084
Debora <ds336756@gmail.com>
2025-05-30 14:00:02 -0300

    Adicionando not null na model do Plano

--- MaisonEcommerce/Models/Plano.cs
+++ MaisonEcommerce/Models/Plano.cs
@@ -5,9 +5,9 @@ namespace MaisonEcommerce.Models
     public class Plano
     {
         public int IdPlano { get; set; }
-        public string Nome { get; set; }
-        public string Descricao { get; set; }
-        public string DuracaoPlano { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        public string? DuracaoPlano { get; set; }
         public decimal Preco { get; set; }
         public DateTime DataCadastro { get; set; }
         public DateTime DataAtualizacao { get; set; }
