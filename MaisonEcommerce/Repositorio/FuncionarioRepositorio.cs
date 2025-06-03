0fb9f5dc9b7a880d75dc8b4b8a795023a4d2b7ae
Debora <ds336756@gmail.com>
2025-06-03 03:24:00 -0300

    Finalizando os repositórios do funcionário e pacote

--- MaisonEcommerce/Repositorio/FuncionarioRepositorio.cs
+++ MaisonEcommerce/Repositorio/FuncionarioRepositorio.cs
@@ -1,11 +1,10 @@
 ﻿using MaisonEcommerce.Models;
 using MySql.Data.MySqlClient;
-using Org.BouncyCastle.Crypto.Prng;
 using System.Data;
 
 namespace MaisonEcommerce.Repositorio
 {
-    public class FuncionarioRepositorio (IConfiguration configuration)
+    public class FuncionarioRepositorio(IConfiguration configuration)
     {
         private readonly string _conexaMySQL = configuration.GetConnectionString("ConexaoMySQL");
         public int Cadastrar(Funcionario funcionario)
@@ -38,16 +37,94 @@ namespace MaisonEcommerce.Repositorio
                     cmd.Parameters.Add("@idade", MySqlDbType.Int32).Value = funcionario.Idade;
                     cmd.Parameters.Add("@sexo", MySqlDbType.VarChar).Value = funcionario.Sexo;
                     cmd.Parameters.Add("@cargo", MySqlDbType.VarChar).Value = funcionario.Cargo;
+
                     int linhasAfetadas = cmd.ExecuteNonQuery();
                     return linhasAfetadas > 0;
                 }
             }
-            catch
+            catch (MySqlException ex)
             {
+                Console.WriteLine($"Erro ao atualizar funcionário: {ex.Message}");
                 return false;
             }
         }
+
+        public IEnumerable<Funcionario> TodosFuncionarios()
+        {
+            List<Funcionario> funcionarios = new List<Funcionario>();
+
+            using (var conexao = new MySqlConnection(_conexaMySQL))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand("select * from tb_Funcionario", conexao);
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                conexao.Close();
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    funcionarios.Add(
+                        new Funcionario
+                        {
+                            IdFuncionario = Convert.ToInt32(dr["IdFuncionario"]),
+                            CPF = ((string)dr["CPF"]),
+                            Nome = ((string)dr["Nome"]),
+                            Idade = Convert.ToInt32(dr["Idade"]),
+                            Sexo = ((string)dr["Sexo"]),
+                            Cargo = ((string)dr["Cargo"]),
+                            DataCadastro = Convert.ToDateTime(dr["DataCadastro"]),
+                            DataAtualizacao = Convert.ToDateTime(dr["DataAtualizacao"])
+                        });
+                }
+                return funcionarios;
+            }
+        }
+
+        public Funcionario ObterFuncionario(int codigo)
+        {
+            using (var conexao = new MySqlConnection(_conexaMySQL))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand("select * from tb_Funcionario where IdFuncionario = @codigo", conexao);
+                cmd.Parameters.AddWithValue("@codigo", codigo);
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                MySqlDataReader dr;
+                Funcionario funcionario = new Funcionario();
+
+                dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+                while (dr.Read())
+                {
+                    funcionario.IdFuncionario = Convert.ToInt32(dr["IdFuncionario"]);
+                    funcionario.CPF = (string)(dr["CPF"]);
+                    funcionario.Nome = (string)(dr["Nome"]);
+                    funcionario.Idade = Convert.ToInt32(dr["Idade"]);
+                    funcionario.Sexo = (string)(dr["Sexo"]);
+                    funcionario.Cargo = (string)(dr["Cargo"]);
+                    
+                }
+                return funcionario;
+            }
+        }
+
+        public void Excluir(int IdFuncionario)
+        {
+            using (var conexao = new MySqlConnection(_conexaMySQL))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand("delete from tb_Funcionario where IdFuncionario = @IdFuncionario", conexao);
+                cmd.Parameters.AddWithValue("@IdFuncionario", IdFuncionario);
+                int linhasAfetadas = cmd.ExecuteNonQuery();
+
+                conexao.Close();
+            }
+        }
     }
-    {
-    }
+    
 }
+
--- MaisonEcommerce/Repositorio/PacoteRepositorio.cs
+++ MaisonEcommerce/Repositorio/PacoteRepositorio.cs
@@ -1,5 +1,6 @@
 ﻿using MaisonEcommerce.Models;
 using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI;
 using Org.BouncyCastle.Crypto.Prng;
 using System.Data;
 
@@ -38,12 +39,84 @@ namespace MaisonEcommerce.Repositorio
                     return linhasAfetadas > 0;
                 }
             }
-            catch
+            catch (MySqlException ex)
             {
+                Console.WriteLine($"Erro ao atualizar pacote: {ex.Message}");
                 return false;
+            }   
+        }
+
+        public IEnumerable<Pacote> TodosPacotes()
+        {
+            List<Pacote> pacotes = new List<Pacote>();
+
+            using (var conexao = new MySqlConnection(_conexaMySQL))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand("select * from tb_Pacote", conexao);
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                conexao.Close();
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    pacotes.Add(
+                        new Pacote
+                        {
+                            IdPacote = Convert.ToInt32(dr["IdPacote"]),
+                            Nome = ((string)dr["Nome"]),
+                            Descricao = ((string)dr["Descricao"]),
+                            Preco = Convert.ToDecimal(dr["Preco"]),
+                            DataCadastro = Convert.ToDateTime(dr["DataCadastro"]),
+                            DataAtualizacao = Convert.ToDateTime(dr["DataAtualizacao"]),
+                        });
+                }
+                return pacotes;
+            }
+        }
+
+        public Pacote ObterPacote(int codigo)
+        {
+            using (var conexao = new MySqlConnection(_conexaMySQL))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand("select * from tb_Pacote where IdPacote=@codigo", conexao);
+                cmd.Parameters.AddWithValue("@codigo", codigo);
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                MySqlDataReader dr;
+                Pacote pacote = new Pacote();
+
+                dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+                while (dr.Read())
+                {
+                    pacote.IdPacote = Convert.ToInt32(dr["IdPacote"]);
+                    pacote.Nome = (string)(dr["Nome"]);
+                    pacote.Descricao = (string)(dr["Descricao"]);
+                    pacote.Preco = Convert.ToDecimal(dr["Preco"]);
+                }
+                return pacote;
+            }
+        }
+
+        public void Excluir(int IdPacote)
+        {
+            using (var conexao = new MySqlConnection(_conexaMySQL))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand("delete from tb_Pacote where IdPacote = @IdPacote", conexao);
+                cmd.Parameters.AddWithValue("@IdPacote", IdPacote);
+                int linhasAfetadas = cmd.ExecuteNonQuery();
+
+                conexao.Close();
             }
         }
     }
-    {
-    }
+    
+    
 }
