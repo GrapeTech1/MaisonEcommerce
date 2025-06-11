8f5530209d4c1d063616bf3e0e7bc0b34c92c444
Bruno <pessoal.conta552@gmail.com>
2025-06-11 16:59:22 -0300

    Criando o ServicoPlanoRepositorio

--- MaisonEcommerce/Repositorio/ServicoPlanoRepositorio.cs
+++ MaisonEcommerce/Repositorio/ServicoPlanoRepositorio.cs
@@ -1,6 +1,35 @@
-﻿namespace MaisonEcommerce.Repositorio
+﻿using MaisonEcommerce.Models;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Data;
+
+
+namespace MaisonEcommerce.Repositorio
 {
-    public class ServicoPlanoRepositorio
+    
+    public class ServicoPlanoRepositorio(IConfiguration configuration) 
     {
+        private readonly string _conexaoMySQL = configuration.GetConnectionString("XonexaoMySQL");
+
+        public int Cadastrar(Servico_Plano servicoPlano)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand("Call insertServicoPlano(@servico, @plano);", conexao);
+                cmd.Parameters.Add("@servico", MySqlDbType.VarChar).Value = servicoPlano.NomeServico;
+                cmd.Parameters.Add("@plano", MySqlDbType.VarChar).Value = servicoPlano.NomePlano;
+                
+                int linhasAfetadas = cmd .ExecuteNonQuery();
+                conexao .Close();
+                return linhasAfetadas;
+            }
+        }
+
+        public bool Atualizar(Servico_Plano ServicoPlano)
+        {
+            try
+        }
     }
 }
