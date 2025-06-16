ebb25d52ab792e2f968a622056151b05df277151
Bruno <pessoal.conta552@gmail.com>
2025-06-16 18:36:08 -0300

    Ajustando erros

--- MaisonEcommerce/Controllers/ServicoPlanoController.cs
+++ MaisonEcommerce/Controllers/ServicoPlanoController.cs
@@ -6,90 +6,90 @@ namespace MaisonEcommerce.Controllers
 {
     public class ServicoPlanoController : Controller
     {
-        //private readonly ServicoPlanoRepositorio _servicoplanoRepositorio;
-        //private readonly ServicoRepositorio _servicoRepositorio;
-        //private readonly PlanoRepositorio _planoRepositorio;
+        private readonly ServicoPlanoRepositorio _servicoplanoRepositorio;
+        private readonly ServicoRepositorio _servicoRepositorio;
+        private readonly PlanoRepositorio _planoRepositorio;
 
-        //public ServicoPlanoController(ServicoPlanoRepositorio servicoplanoRepositorio, ServicoRepositorio servicoRepositorio, PlanoRepositorio planoRepositorio)
-        //{
-        //    _servicoplanoRepositorio = servicoplanoRepositorio;
-        //    _servicoRepositorio = servicoRepositorio;
-        //    _planoRepositorio = planoRepositorio;
-        //}
+        public ServicoPlanoController(ServicoPlanoRepositorio servicoplanoRepositorio, ServicoRepositorio servicoRepositorio, PlanoRepositorio planoRepositorio)
+        {
+            _servicoplanoRepositorio = servicoplanoRepositorio;
+            _servicoRepositorio = servicoRepositorio;
+            _planoRepositorio = planoRepositorio;
+        }
 
-        //public IActionResult Index()
-        //{
-        //    return View(_servicoplanoRepositorio.TodosServicoPlano());
-        //}
+        public IActionResult Index()
+        {
+            return View(_servicoplanoRepositorio.TodosServicoPlano());
+        }
 
-        //public IActionResult CadastrarServicoPlano()
-        //{
-        //    ViewBag.servicos = _servicoRepositorio.TodosServicos();
-        //    ViewBag.planos = _planoRepositorio.TodosPlanos();
+        public IActionResult CadastrarServicoPlano()
+        {
+            ViewBag.servicos = _servicoRepositorio.TodosServicos();
+            ViewBag.planos = _planoRepositorio.TodosPlanos();
 
-        //    return View(new Servico_Plano());
-        //}
+            return View(new Servico_Plano());
+        }
 
-        //[HttpPost]
-        //public IActionResult CadastrarServicoPlano(Servico_Plano servicoPlano)
-        //{
-        //    int linhasAfetadas = _servicoplanoRepositorio.Cadrastrar(servicoPlano);
+        [HttpPost]
+        public IActionResult CadastrarServicoPlano(Servico_Plano servicoPlano)
+        {
+            int linhasAfetadas = _servicoplanoRepositorio.Cadastrar(servicoPlano);
 
-        //    return RedirectToAction(nameof(Index));
-        //}
+            return RedirectToAction(nameof(Index));
+        }
 
-        //public IActionResult EditarServicoPlano(int id)
-        //{
-        //    var servicoPlano = _servicoplanoRepositorio.ObterServicoPlano(id);
+        public IActionResult EditarServicoPlano(int id)
+        {
+            var servicoPlano = _servicoplanoRepositorio.ObterServicoPlano(id);
 
-        //    if (servicoPlano == null)
-        //    {
-        //        return NotFound();
-        //    }
+            if (servicoPlano == null)
+            {
+                return NotFound();
+            }
 
-        //    ViewBag.servicos = _servicoRepositorio.TodosServicos();
-        //    ViewBag.planos = _planoRepositorio.TodosPlanos();
+            ViewBag.servicos = _servicoRepositorio.TodosServicos();
+            ViewBag.planos = _planoRepositorio.TodosPlanos();
 
-        //    return View(servicoPlano);
-        //}
+            return View(servicoPlano);
+        }
 
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
 
-        //public IActionResult EditarServicoPlano(int id, [Bind("IdServicoPlano, IdServico, IdPlano NomeServico, NomePlano")] Servico_Plano servicoPlano)
-        //{
-        //    if (id != servicoPlano.IdServicoPlano)
-        //    {
-        //        return BadRequest();
-        //    }
-        //    if (ModelState.IsValid)
-        //    {
-        //        try
-        //        {
-        //            if (_servicoPlanoRepositorio.Atualizar(servicoPlano))
-        //            {
-        //                TempData["Mensagem"] = "Servico inserido em pacote com sucesso!";
-        //                TempData["Classe"] = "alert alert-success";
-        //                return RedirectToAction(nameof(Index));
-        //            }
-        //        }
-        //        catch (Exception)
-        //        {
-        //            ModelState.AddModelError("", "Erro ao editar pacote");
-        //            return View(servicoPlano);
-        //        }
-        //    }
-        //    return View(servicoPlano);
-        //}
-        //public IActionResult ExcluirServicoPlano(int id)
-        //{
-        //    _servicoplanoRepositorio.Excluir(id);
+        public IActionResult EditarServicoPlano(int id, [Bind("IdServicoPlano, IdServico, IdPlano, NomeServico, NomePlano")] Servico_Plano servicoPlano)
+        {
+            if (id != servicoPlano.IdServicoPlano)
+            {
+                return BadRequest();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (_servicoplanoRepositorio.Atualizar(servicoPlano))
+                    {
+                        TempData["Mensagem"] = "Servico inserido em pacote com sucesso!";
+                        TempData["Classe"] = "alert alert-success";
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+                catch (Exception)
+                {
+                    ModelState.AddModelError("", "Erro ao editar pacote");
+                    return View(servicoPlano);
+                }
+            }
+            return View(servicoPlano);
+        }
+        public IActionResult ExcluirServicoPlano(int id)
+        {
+            _servicoplanoRepositorio.Excluir(id);
 
 
-        //    TempData["Mensagem"] = $"Serviço retirado de plano com sucesso!  -  {DateTime.Now}";
-        //    TempData["Classe"] = "alert alert-success";
+            TempData["Mensagem"] = $"Serviço retirado de plano com sucesso!  -  {DateTime.Now}";
+            TempData["Classe"] = "alert alert-success";
 
-        //    return RedirectToAction(nameof(Index));
-        //}
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
