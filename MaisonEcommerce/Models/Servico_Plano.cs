1b84ac7a24ee1120dea3747b7c03ec2db416651d
Bruno <pessoal.conta552@gmail.com>
2025-06-16 17:10:12 -0300

    Arrumando o repositorio

--- MaisonEcommerce/Models/Servico_Plano.cs
+++ MaisonEcommerce/Models/Servico_Plano.cs
@@ -5,5 +5,8 @@
         public int IdServicoPlano { get; set; }
         public int IdServico { get; set; } // foreign key para a tabela Servico
         public int IdPlano { get; set; } // foreign key para a tabela Plano
+
+        public string NomeServico { get; set; }
+        public string NomePlano { get; set; }
     }
 }
--- MaisonEcommerce/Repositorio/ServicoPlanoRepositorio.cs
+++ MaisonEcommerce/Repositorio/ServicoPlanoRepositorio.cs
@@ -9,61 +9,117 @@ namespace MaisonEcommerce.Repositorio
 
     public class ServicoPlanoRepositorio(IConfiguration configuration)
     {
-        //private readonly string _conexaoMySQL = configuration.GetConnectionString("XonexaoMySQL");
+        private readonly string _conexaoMySQL = configuration.GetConnectionString("XonexaoMySQL");
 
-        //public int Cadastrar(Servico_Plano servicoPlano)
-        //{
-        //    using (var conexao = new MySqlConnection(_conexaoMySQL))
-        //    {
-        //        conexao.Open();
+        public int Cadastrar(Servico_Plano servicoPlano)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
 
-        //        MySqlCommand cmd = new MySqlCommand("Call insertServicoPlano(@servico, @plano);", conexao);
-        //        cmd.Parameters.Add("@servico", MySqlDbType.VarChar).Value = servicoPlano.NomeServico;
-        //        cmd.Parameters.Add("@plano", MySqlDbType.VarChar).Value = servicoPlano.NomePlano;
+                MySqlCommand cmd = new MySqlCommand("Call insertServicoPlano(@servico, @plano);", conexao);
+                cmd.Parameters.Add("@servico", MySqlDbType.VarChar).Value = servicoPlano.NomeServico;
+                cmd.Parameters.Add("@plano", MySqlDbType.VarChar).Value = servicoPlano.NomePlano;
 
-        //        int linhasAfetadas = cmd.ExecuteNonQuery();
-        //        conexao.Close();
-        //        return linhasAfetadas;
-        //    }
-        //}
+                int linhasAfetadas = cmd.ExecuteNonQuery();
+                conexao.Close();
+                return linhasAfetadas;
+            }
+        }
 
-        //public bool Atualizar(Servico_Plano ServicoPlano)
-        //{
-        //    try
-        //    {
-        //        using (var conexao = new MySqlConnection(_conexaoMySQL))
-        //        {
-        //            conexao.Open();
+        public bool Atualizar(Servico_Plano ServicoPlano)
+        {
+            try
+            {
+                using (var conexao = new MySqlConnection(_conexaoMySQL))
+                {
+                    conexao.Open();
 
-        //            MySqlCommand cmd = new MySqlCommand("Update tb_Servico_Plano set IdServico=@servico, IdPlano@plano where IdServicoPlano=@idServico", conexao);
-        //            cmd.Parameters.Add("@idServicoPlano", MySqlDbType.Int32).Value = ServicoPlano.IdServicoPlano;
-        //            cmd.Parameters.Add("@servico", MySqlDbType.Int32).Value = ServicoPlano.NomeServico;
-        //            cmd.Parameters.Add("@plano", MySqlDbType.Int32).Value = ServicoPlano.NomePlano;
+                    MySqlCommand cmd = new MySqlCommand("Update tb_Servico_Plano set IdServico=@servico, IdPlano@plano where IdServicoPlano=@idServico", conexao);
+                    cmd.Parameters.Add("@idServicoPlano", MySqlDbType.Int32).Value = ServicoPlano.IdServicoPlano;
+                    cmd.Parameters.Add("@servico", MySqlDbType.Int32).Value = ServicoPlano.NomeServico;
+                    cmd.Parameters.Add("@plano", MySqlDbType.Int32).Value = ServicoPlano.NomePlano;
 
-        //            int linhasAfetadas = cmd.ExecuteNonQuery();
-        //            return linhasAfetadas > 0;
-        //        }
-        //    }
+                    int linhasAfetadas = cmd.ExecuteNonQuery();
+                    return linhasAfetadas > 0;
+                }
+            }
 
-        //    catch (MySqlException ex)
-        //    {
-        //        Console.WriteLine($"Erro ao alterar serviço-plano: {ex.Message}");
-        //        return false;
-        //    }
-        //}
+            catch (MySqlException ex)
+            {
+                Console.WriteLine($"Erro ao alterar serviço-plano: {ex.Message}");
+                return false;
+            }
+        }
 
-        //public IEnumerable<Servico_Plano> TodosServicoPlano()
-        //{
-        //    List<Servico_Plano> servico_Plamos = new List<Servico_Plano>();
+        public IEnumerable<Servico_Plano> TodosServicoPlano()
+        {
+            List<Servico_Plano> servico_Plamos = new List<Servico_Plano>();
 
-        //    using (var conexao = new MySqlConnection(_conexaoMySQL))
-        //    {
-        //        conexao.Open();
-        //        MySqlCommand cmd = new MySqlCommand("select tb_Servico_Plano.IdServicoPlano, tb_Servico.Nome as Nome_Servico, tb_Plano.Nome as Nome_Plano \r\n" +
-        //            "fromt tb_Plano, tb_Servico, tb_Servico_Plano\r\n" +
-        //            "where tb_Servico_Plano");
-        //    }
-        //}
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("select tb_Servico_Plano.IdServicoPlano, tb_Servico.Nome as Nome_Servico, tb_Plano.Nome as Nome_Plano \r\n" +
+                    "fromt tb_Plano, tb_Servico, tb_Servico_Plano\r\n" +
+                    "where tb_Servico_Plano.IdPlano = tb_Plano.IdPlano and tb_ServicoPlano.IdServico = tb_Servico.IdServico;", conexao);
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                conexao.Close();
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    Servico_Plano.Add(
+                        new Servico_Plano
+                        {
+                            IdServicoPlano = Convert.ToInt32(dr["IdServicoPlano"]),
+                            NomeServico = ((string)dr["Nome_Serviço"]),
+                            NomePlano = ((string)dr["Nome_Plano"])
+                        });
+                }
+                return servico_Plamos;
+            }
+        }
+
+        public Servico_Plano ObterServicoPlano(int codigo)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand("select * from tb_Servico_Plano where IdServicoPlano = @codigo", conexao);
+                cmd.Parameters.AddWithValue("@codigo", codigo);
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                MySqlDataReader dr;
+                Servico_Pacote servicoPlano = new Servico_Plano();
+
+                dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+                while (dr.Read())
+                {
+                    servicoPlano.IdServicoPacote = Convert.ToInt32(dr["IdServicoPlano"]);
+                    servicoPlano.IdPlano = Convert.ToInt32(dr["IdPlano"]);
+                    servicoPlano.IdServico = Convert.ToInt32(dr["IdServico"]);
+                }
+                return servicoPlano;
+            }
+        }
+
+        public void Excluir(int IdServicoPlano)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand("delee from tb_Servico_Plano where IdServicoPlano = @idServicoPlano", conexao);
+                int linhasAfetadas = cmd .ExecuteNonQuery();
+
+                conexao .Close();
+            }
+
+        }
     }
 
-   }
+}
